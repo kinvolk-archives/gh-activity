47ae61e77b63042595239ced077add5ba2972bb7
Irena Benja <irena@kinvolk.al>
2016-07-07 16:57:14 +0200

    Refactor code into functions
    This commit prepares us for accepting parameters from the command
    line.

--- api.go
+++ api.go
@@ -8,68 +8,87 @@ import (
 	"net/http"
 )
 
+type Actor struct {
+	Login        string `json:"login"`
+	DisplayLogin string `json:"display_login"`
+	GravatarId   string `json:"gravatar_id"`
+	Url          string `json:"url`
+	AvatarUrl    string `json:"avatar_url"`
+}
+
+type Repo struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Url  string `json:"url"`
+}
+
+type Author struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+type Commits struct {
+	Sha      string `json:"sha"`
+	Author   Author `json:"author"`
+	Message  string `json:"message"`
+	Distinct bool   `json:"distinct"`
+	Url      string `json:"url"`
+}
+type Payload struct {
+	PushId       int    `json:"push_id"`
+	Size         int    `json:"size"`
+	DistinctSize int    `json:"distinct_size"`
+	Ref          string `json:"ref"`
+	Head         string `json:"head"`
+	Before       string `json:"before"`
+	Commits      []Commits
+}
+type Org struct {
+	Id         int    `json:"id"`
+	Login      string `json:"login"`
+	GravatarId string `json:"gravatar_id"`
+	Url        string `json:"url"`
+	AvatarUrl  string `json:"avatar_url"`
+}
+
+//Eventcoding json
+type Event struct {
+	Id           string  `json:"id"`
+	Type         string  `json:"type"`
+	Actor        Actor   `json:"actor"`
+	Repo         Repo    `json:"repo"`
+	Payload      Payload `json:"payload"`
+	Public       bool    `json:"public"`
+	CreatedAt    string  `json:"created_at"`
+	Organization Org     `json:"org"`
+}
+
+type Activity struct {
+	Id        string `json:"id"`
+	Person    string `json:"person"`
+	Repo      string `json:"repo"`
+	CreatedAt string `json:"created_at"`
+}
+
 func main() {
-	// get the data
-	resp, err := http.Get("https://api.github.com/users/kinvolk/events")
+	e, err := getEvents("kinvolk")
 	if err != nil {
-		fmt.Printf("error getting the data from github", err)
+		fmt.Printf("problem getting events: %v", err)
 		return
 	}
 
-	type Actor struct {
-		Id           int    `json:"id"`
-		Login        string `json:"login"`
-		DisplayLogin string `json:"display_login"`
-		GravatarId   string `json:"gravatar_id"`
-		Url          string `json:"url`
-		AvatarUrl    string `json:"avatar_url"`
+	err = writeActivity("event.json", e)
+	if err != nil {
+		fmt.Printf("problem writing the activities to file: %v", err)
+		return
 	}
+}
 
-	type Repo struct {
-		Id   int    `json:"id"`
-		Name string `json:"name"`
-		Url  string `json:"url"`
-	}
-
-	type Author struct {
-		Email string `json:"email"`
-		Name  string `json:"name"`
-	}
-
-	type Commits struct {
-		Sha      string `json:"sha"`
-		Author   Author `json:"author"`
-		Message  string `json:"message"`
-		Distinct bool   `json:"distinct"`
-		Url      string `json:"url"`
-	}
-	type Payload struct {
-		PushId       int    `json:"push_id"`
-		Size         int    `json:"size"`
-		DistinctSize int    `json:"distinct_size"`
-		Ref          string `json:"ref"`
-		Head         string `json:"head"`
-		Before       string `json:"before"`
-		Commits      []Commits
-	}
-	type Org struct {
-		Id         int    `json:"id"`
-		Login      string `json:"login"`
-		GravatarId string `json:"gravatar_id"`
-		Url        string `json:"url"`
-		AvatarUrl  string `json:"avatar_url"`
-	}
-
-	//Eventcoding json
-	type Event struct {
-		Id           string  `json:"id"`
-		Type         string  `json:"type"`
-		Actor        Actor   `json:"actor"`
-		Repo         Repo    `json:"repo"`
-		Payload      Payload `json:"payload"`
-		Public       bool    `json:"public"`
-		CreatedAt    string  `json:"created_at"`
-		Organization Org     `json:"org"`
+func getEvents(org string) ([]Event, error) {
+	// get the data
+	resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s/events", org))
+	if err != nil {
+		return nil, fmt.Errorf("error getting the data from github: %v", err)
 	}
 
 	buf := new(bytes.Buffer)
@@ -78,19 +97,12 @@ func main() {
 	var e []Event
 	//saving the data to the new format
 	if err := json.Unmarshal(buf.Bytes(), &e); err != nil {
-		fmt.Printf("problem unmarshalling data: %v\n", err)
-		return
+		return nil, fmt.Errorf("problem unmarshalling data: %v", err)
 	}
+	return e, nil
+}
 
-	fmt.Printf("marshalled data:\n %#v\n", e)
-
-	type Activity struct {
-		Id        string `json:"id"`
-		Person    string `json:"person"`
-		Repo      string `json:"repo"`
-		CreatedAt string `json:"created_at"`
-	}
-
+func writeActivity(outfile string, e []Event) error {
 	var activities []Activity
 	for _, a := range e {
 		act := Activity{
@@ -103,8 +115,9 @@ func main() {
 	}
 
 	b, err := json.Marshal(activities)
-
-	if err = ioutil.WriteFile("myjson.json", b, 0666); err != nil {
-		fmt.Printf("Failed to write the file in json format", err)
+	if err = ioutil.WriteFile(outfile, b, 0666); err != nil {
+		return fmt.Errorf("failed to write the file in json format: %v", err)
 	}
+
+	return nil
 }
